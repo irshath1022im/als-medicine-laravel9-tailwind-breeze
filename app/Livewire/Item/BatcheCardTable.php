92068364bb239fcaf9a5b84800cb0d25f91c4369
irshath1022im <im.irshath1022@gmail.com>
2025-04-16 13:44:46 +0300

    batch item created

---
+++ app/Livewire/Item/BatcheCardTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Item;
+
+use Livewire\Component;
+
+class BatcheCardTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.item.batche-card-table');
+    }
+}
---
+++ resources/views/livewire/item/batche-card-table.blade.php
@@ -0,0 +1,48 @@
+<div>
+
+
+
+    <div class="table-overflow">
+        <table class="table ">
+            <thead class="table-head">
+                <th class="table-th">ITEM NAME</th>
+                <th class="table-th">Qty</th>
+                <th class="table-th">EXPIRY DATE</th>
+                <th class="table-th">DAYS REMAINING</th>
+            </thead>
+
+            <tbody>
+                <tr class="table-tr">
+                    <td class="table-td ">ITEM1</td>
+                    <td class="table-td">10</td>
+                    <td class="table-td">2025-10-25</td>
+                    <td class="table-td">25</td>
+                </tr>
+            </tbody>
+
+
+        </table>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
